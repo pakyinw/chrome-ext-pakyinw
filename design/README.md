d527bf0f4a0985ba56540ad96f7d3365cea08610
pakyin-wong <pakyin.wong@link.cuhk.edu.hk>
2019-03-07 05:21:50 +0800

    CSV Export Function Added

---
+++ design/README.md
@@ -0,0 +1,4 @@
+# Reference
+https://stackoverflow.com/questions/15551779/open-link-in-new-tab-or-window
+https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+https://stackoverflow.com/questions/36324333/refused-to-execute-inline-event-handler-because-it-violates-csp-sandbox
--- popup.js
+++ popup.js
@@ -3,6 +3,28 @@ var getDatetime = (t) => {
     return new Date(Number(t)).toLocaleString()
 }
 
+var exportCSV = () => {
+    let csvContent = "data:text/csv;charset=utf-8,";
+    chrome.storage.sync.get(null,(items)=>{
+        var allKeys = Object.keys(items);
+        if (allKeys.length > 0){
+            allKeys.forEach((key,index)=>{
+                csvContent += getDatetime(key) + ',' + items[key].text + ',' + items[key].url + "\r\n";
+            })
+        }
+    });
+    
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "vocab_" + new Date().toLocaleDateString() + ".csv");
+    document.body.appendChild(link); // Required for FF
+    
+    link.click();
+}
+
+document.getElementById("btnExport").addEventListener("click", exportCSV);
+
 chrome.storage.sync.get(null,(items)=>{
     var allKeys = Object.keys(items);
     if (allKeys.length > 0){
@@ -11,7 +33,7 @@ chrome.storage.sync.get(null,(items)=>{
             console.log("time: " + getDatetime(key))
             console.log("text: " + items[key].text)
             console.log("url: " + items[key].url)
-            document.getElementById("divTableBody").innerHTML += "<div class='divTableRow'><div class='divTableCell'>" + getDatetime(key) +"</div><div class='divTableCell'>" + items[key].text + "</div><div class='divTableCell'><a href='" +items[key].url + "'>" + items[key].url + "</a></div></div>"
+            document.getElementById("divTableBody").innerHTML += "<div class='divTableRow'><div class='divTableCell'>" + getDatetime(key) +"</div><div class='divTableCell'>" + items[key].text + "</div><div class='divTableCell'><a target='_blank' rel='noopener noreferrer' href='" +items[key].url + "'>here</a></div></div>"
         })
         
     }
